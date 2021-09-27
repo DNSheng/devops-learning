63127a3c43393057c9aaef958c221234180d4be6
Dan Sheng <dnsheng@gmail.com>
2021-09-27 15:40:03 -0400

    Updated pylint action

--- myprogram/__main__.py
+++ myprogram/__main__.py
@@ -8,19 +8,19 @@ import pandas
 
 
 def main():
-	"""Run the main function"""
-	print("Hello. The program has started to run.")
-	print(f"The circumference of a circle of r=3 is {circumference(3)}")
-	print(f"The pandas version installed is: {pandas.__version__}")
+    """Run the main function"""
+    print("Hello. The program has started to run.")
+    print(f"The circumference of a circle of r=3 is {circumference(3)}")
+    print(f"The pandas version installed is: {pandas.__version__}")
 
 
 def circumference(radius):
-	"""Return the circumference to 2 decimal places
+    """Return the circumference to 2 decimal places
 
-	c = 2r * pi
-	"""
-	return round(2 * radius * math.pi, 2)
-	
+    c = 2r * pi
+    """
+    return round(2 * radius * math.pi, 2)
+
 
 if __name__ == '__main__':
-	main()
+    main()
