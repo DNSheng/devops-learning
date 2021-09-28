ab1d6653064a42d216406895e0e4bd8e631b3130
Dan Sheng <dnsheng@gmail.com>
2021-09-28 09:35:10 -0400

    Fixed setup.py indentation

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 import myprogram.constants as constants
 
 setup(
-	name="myprogram",
-	version=constants.VERSION,
-	description="A simple python program",
-	packages=find_packages(),
+    name="myprogram",
+    version=constants.VERSION,
+    description="A simple python program",
+    packages=find_packages(),
 
-	install_requires=[
-		"pandas",
+    install_requires=[
+        "pandas",
         "flask",
-	],
+    ],
 
-	entry_points={
-		'console_scripts': [
-			'myprogram=myprogram.__main__:main',
-		],
-	},
+    entry_points={
+        'console_scripts': [
+            'myprogram=myprogram.__main__:main',
+        ],
+    },
 )
